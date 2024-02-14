6559cdf3211cfa211526ced1a9ebc8656b36ee6f
LeeroyTJ <jumbethabol@gmail.com>
2024-02-14 15:23:34 +0200

    added two new functions - isWinner() and playGame() - that lets the Player/User play 5 rounds of the game before displaying the overall winner

--- script.js
+++ script.js
@@ -12,27 +12,52 @@ function getComputerChoice() {
             return "Scissors";
             break;
     }
-
 }
 
-function rockPaperScissors(playerSelection, computerSelection) {
+let winnerIsPlayer = 0;
+let winnerIsComp = 0;
+
+function playRound(playerSelection, computerSelection) {
 
     let playerInput = prompt("Rock, Paper or Scissors? ");
     playerInput = playerInput[0].toUpperCase() + playerInput.slice(1).toLowerCase();
 
-    let compInput = getComputerChoice();
+    let compInput = getComputerChoice();    
 
-    if (playerInput === compInput) {
+    if (playerInput === compInput) {  
         alert("It's a tie! Please go again.");
     }
+
     else if (playerInput === "Rock" && compInput === "Scissors" || 
     playerInput === "Paper" && compInput === "Rock" || 
-    playerInput === "Scissors" && compInput === "Paper") {
-        alert(`Yaay, you won! ${playerInput} beats ${compInput}`);
-    }
-    else {
-        alert(`You lose! ${compInput} beats ${playerInput}`);
+    playerInput === "Scissors" && compInput === "Paper") {        
+        winnerIsPlayer = winnerIsPlayer + 1;
+        alert(`Yay, you won! ${playerInput} beats ${compInput}\nPlayer score: ${winnerIsPlayer}`);
     }
 
+    else {        
+        winnerIsComp = winnerIsComp + 1;
+        alert(`You lose! ${compInput} beats ${playerInput}\nComputer score: ${winnerIsComp}`);
+    }
 }
-console.log(rockPaperScissors());
+
+function isWinner() {
+    if (winnerIsPlayer > winnerIsComp) {
+        alert(`Player ${winnerIsPlayer} - Computer ${winnerIsComp}\nPlayer WINS! Hooray!!!!`);
+    }
+    else if (winnerIsPlayer < winnerIsComp) {
+        alert(`Player ${winnerIsPlayer} - Computer ${winnerIsComp}\nSorry, Computer wins. But hey, better luck next time!`);
+    }
+    else {
+        alert(`Player ${winnerIsPlayer} - Computer ${winnerIsComp}\nIt's a DRAW. What a tight game it was.`);
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+    console.log(isWinner());
+}
+
+console.log(playGame());
